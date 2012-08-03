5921984aa0d7433e200e586f8a359ca0c6347ae4
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-08-03 07:17:52 +0300

    Initial commit: 1. Init repository. 2. Add git file ignoring rules. 3. Write a basic markdown to html converter.

---
+++ pythentation/__init__.py
@@ -0,0 +1,30 @@
+'''Simple utility to make a pretty presentations from markdown text
+'''
+import markdown
+import sys
+
+
+def generate(text):
+    return markdown.markdown(text)
+
+
+def main():
+    argc = len(sys.argv)
+    if argc < 2:
+        raise Exception('No input file specified.')
+    elif argc > 3:
+        raise Exception('To many arguments.')
+    elif argc == 1:
+        input_file = sys.argv[1]
+        output_file = input_file + '.html'
+    else:
+        input_file, output_file = sys.argv[1:]
+    text = ''.join(open(input_file).readlines())
+    html = generate(text)
+    write_handle = open(output_file, 'w')
+    write_handle.write(html)
+    write_handle.close()
+
+
+if __name__ == '__main__':
+    main()
