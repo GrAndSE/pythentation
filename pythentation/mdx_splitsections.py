8300719c5a071ea72b0dd863619f72ea82b4d0bb
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-08-03 15:25:27 +0300

    Add a table of contents

--- pythentation/mdx_splitsections.py
+++ pythentation/mdx_splitsections.py
@@ -2,6 +2,14 @@
 '''
 import markdown
 import re
+import unidecode
+
+
+def slugify(str):
+    '''To slug
+    '''
+    str = unidecode.unidecode(str).lower()
+    return re.sub(r'\W+', '-', str)
 
 
 def split_sections(tree, tag, slide_class, head_class=None):
@@ -9,22 +17,32 @@ def split_sections(tree, tag, slide_class, head_class=None):
     '''
     head_class = head_class or slide_class
     wrapper = None
+    toc = markdown.util.etree.Element('nav')  # create a table of content
     children = list(tree)
     for i, child in enumerate(children):
         # should allow lower level to stop as well
         m = re.search(r"h(\d+)", child.tag)
         tag_level = int(m.group(1)) if m else 0
         if tag_level == 1 or tag_level == 2:
-            # Create a new wrapper
+            # Create a new wrapper to start new section
             wrapper = markdown.util.etree.Element(tag)
             wrapper.set('class', head_class if tag_level == 1 else slide_class)
+            wrapper.set('id', slugify(child.text))
             tree.remove(child)
             tree.insert(i, wrapper)
             wrapper.append(child)
+            # Add into table of content
+            toc_item = markdown.util.etree.Element('a')
+            toc_item.text = child.text
+            toc_item.set('href', '#' + wrapper.get('id'))
+            toc_item.set('title', child.text)
+            toc.append(toc_item)
         elif wrapper:
             # Add an element into exists wrapper
             tree.remove(child)
             wrapper.append(child)
+    # Put a table of content as first element
+    tree.insert(0, toc)
 
 
 class SplitSectionsTreeprocessor(markdown.treeprocessors.Treeprocessor):
